04cdd0e59a4c4a6270f1507253d8c69ad5b341a3
tkido <tkido@xevo.com>
2021-04-30 09:32:24 +0900

    update

--- exportmanager.go
+++ exportmanager.go
@@ -41,22 +41,6 @@ func (m *ExportManager) Export(faceNum int) {
 	facePath := filepath.Join(".", m.Root, "faces", fmt.Sprintf("%04d.png", g.Character.Id))
 	m.SaveImage(facePath, m.Face)
 
-	// for i, st := range status.FaceTypes {
-	// 	if int(st)%8 == 0 {
-	// 		m.Face, _ = ebiten.NewImage(144*4, 144*2, ebiten.FilterDefault)
-	// 	}
-	// 	g.StatusMenu.Status = st
-	// 	g.Sprites.reflesh(sprite.Face)
-	// 	op := &ebiten.DrawImageOptions{}
-	// 	x, y := i%8/2, i%8%2
-	// 	op.GeoM.Translate(float64(x)*144, float64(y)*144)
-	// 	m.Face.DrawImage(g.Sprites.Face, op)
-	// 	if int(st)%8 == 7 {
-	// 		facePath := filepath.Join(".", m.Root, "faces", fmt.Sprintf("%04d_%02d.png", g.Character.Id, int(st)/8))
-	// 		m.SaveImage(facePath, m.Face)
-	// 	}
-	// }
-
 	// SV
 	for _, st := range status.Types {
 		g.StatusMenu.Status = st
